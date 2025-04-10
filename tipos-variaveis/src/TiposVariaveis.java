bca2ad9b892178221deeb5a1cafec7554a437013
jefersonrds <contatojefersonrds@gmail.com>
2025-04-09 23:20:25 -0300

    Feat: Tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
+import java.sql.Date;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int idade = 25;
+        long grande = 1324545;
+        double salario = 1500.50;
+        boolean isEstudante = true;
+        char genero = 'M';
+        String nome = "João";
+
     }
 }
