68dea0d162ab86648accd3f48df1fa49c0a898fa
Jefersonrds <contatojefersonrds@gmail.com>
2025-04-18 12:21:11 -0300

    Feat: Tratar error de inputs

---
+++ controle-fluxo/src/AboutMe.java
@@ -0,0 +1,41 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+
+public class AboutMe{
+
+   
+    public static void main(String[] args) {
+        
+     
+        //Tranto as excessoes usando try
+        try{
+
+            //Criando objeto Scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Por favor, digite seu nome:");
+            String nome = scanner.nextLine();
+
+            System.out.println("Agora, digite seu sobrenome:");
+            String sobrenome = scanner.nextLine();
+
+            System.out.println("Informe sua idade:");
+            int idade = scanner.nextInt();
+
+            //Imprimindo os dados
+            System.out.println("Olá " + nome +" " + sobrenome + ", sua idade e: " + idade);
+
+            scanner.close();
+        }
+
+        //Tratando o erro
+        catch(InputMismatchException e){
+
+            System.out.println("O campo de idade deve ser numerico");
+
+        }
+    }
+}
--- controle-fluxo/src/Tabuada.java
+++ controle-fluxo/src/Tabuada.java
@@ -25,6 +25,14 @@ public class Tabuada {
         
         
         scanner.close();
+
+        for(int x=1;x<=10;x++){
+            if ((x % 2 ==0)) {
+                System.out.println(x);
+            }
+        }
     }
+
+
 
 }
